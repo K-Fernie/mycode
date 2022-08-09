a0be32551cd70c24fd65ee4918ec58fc8cb2d7e2
Katy Fernelius <katyfernelius@users.noreply.github.com>
2022-08-09 17:28:40 +0000

    simple python script created

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""Alta3 Research | RZFeeser
+    print() - concatenate vs print a series of items"""
+
+
+def main():
+
+    user_input = input("Please enter an IPv4 IP address:")
+
+    print("You told me the IPv4 address is:", user_input)
+
+    vendor_name = input("Please enter the vendor name of the device:")
+
+    print("You told me the vendor name is:", vendor_name)
+
+main()
