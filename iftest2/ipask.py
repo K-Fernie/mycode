4e5d9b13df8d082c7e70631c0d8ff462fcd78f41
Katy Fernelius <katyfernelius@users.noreply.github.com>
2022-08-10 14:36:11 +0000

    working on if, elif, else logic

---
+++ iftest2/ipask.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Alta3 Research | RZFeeser 
+    Conditionals - testing if strings are true"""
+
+
+ipchk = input("Apply an IP address: ")
+
+if ipchk: 
+    print("Looks like the IP address was set: " + ipchk)
+else: 
+    print("You did not provide an input.")
