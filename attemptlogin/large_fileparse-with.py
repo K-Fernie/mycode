f3bf0fcd8236761490f6c16de7c0b9aa4eeb497d
Katy Fernelius <katyfernelius@users.noreply.github.com>
2022-08-10 18:34:02 +0000

    Learning to parse log files /come back to this

---
+++ attemptlogin/large_fileparse-with.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+loginfail = 0
+
+with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
+
+    for line in kfile:
+
+        if "- - - - -] Authorization failed" in line:
+            loginfail += 1
+
+print("The number of failed log in attempts is", loginfail)
+
