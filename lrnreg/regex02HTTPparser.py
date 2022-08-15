c1b8e224b39a1ca445823b8d1158ae7a26a209ce
Katy Fernelius <katyfernelius@users.noreply.github.com>
2022-08-15 16:40:45 +0000

    Learning about regular expression

--- lrnreg/regex02HTTPparser.py
+++ lrnreg/regex02HTTPparser.py
@@ -6,21 +6,31 @@ import requests
 
 def main():
 
+    while True: 
+        count =0
+        print("Where should we search?")
 
-    print("Where should we search?")
+        url = input("> ")
+    
+        print(f"Great! So we'll try to open this URL {url} to search for the phrase:")
+        searchFor = input("> ")
 
-    url = input("> ")
+        resp = requests.get(url)
+        searchMe = resp.text
+        print(type(searchMe))
+        if re.search(searchFor, searchMe):
+            print("Found a match!")
+            for x in searchMe.split(): 
+                if x == searchFor: 
+                    count+=1
+            print(f"There are {count} occurances of that term")
+        else: 
+            print("No match!")
 
-    print(f"Great! So we'll try to open this URL {url} to search for the phrase:")
-    searchFor = input("> ")
-
-    resp = requests.get(url)
-    searchMe = resp.text
-
-    if re.search(searchFor, searchMe):
-        print("Found a match!")
-    else: 
-        print("No match!")
+        
+        contQ = input("Would you like to search again? (enter q if no): ")
+        if contQ == 'q':
+            break
 
 if __name__ == "__main__":
     main()
