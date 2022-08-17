7f60ed29ca1db507dd5ec5edadfc9117d321cecd
Katy Fernelius <katyfernelius@users.noreply.github.com>
2022-08-17 17:02:44 +0000

    Exploring how to work with large data sets

---
+++ API/iceAndFire04.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pprint
+import requests
+
+AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
+
+def main():
+    
+    got_charToLookup = input("Pick a number between 1 and 1000 to return info on a GoT character! ")
+
+    gotresp = requests.get(AOIF_CHAR + got_charToLookup)
+
+    got_dj = gotresp.json()
+
+    pprint.pprint(got_dj)
+
+if __name__ == "__main__":
+    main()
